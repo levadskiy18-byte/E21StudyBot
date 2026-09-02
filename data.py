4cd2c03e1d8531fc155a44137eed911c091bdb94
levadskiy18-byte <levadskiy18@gmail.com>
2026-09-02 08:32:55 +0300

    Update data.py

--- data.py
+++ data.py
@@ -1,87 +1,53 @@
-# База данных предметов, преподавателей и ссылок
-SUBJECTS = {
-    "Українська мова та література": {
-        "teacher": "Бречко Олена Миколаївна",
-        "classroom": "https://classroom.google.com/c/ODc2NDI2NTM0NzYw?cjc=j7foncpe",
-        "zoom": "https://us04web.zoom.us/j/5546658153?pwd=eUR0cnF2Y3pIUHozeHhjV09XOU9OQT09"
-    },
-    "Виховні години та фізичне виховання": {
-        "teacher": "Шевченко Тетяна Валеріївна",
-        "classroom": "https://classroom.google.com/c/ODAxMDU3MTA0Mzk5?cjc=p4c75e2n",
-        "zoom": "https://us05web.zoom.us/j/87000857117?pwd=VXduMys0V3FhanJQY0VxdUU0QllwQT09"
-    },
-    "Фізика": {
-        "teacher": "Табачник Олена Миколаївна",
-        "classroom": "https://classroom.google.com/c/ODc2NTQ3Mjk0NTMx?cjc=42m7a3h6",
-        "zoom": "https://us05web.zoom.us/j/5433319306?pwd=R1VQMFlHbFY3UGRSS0p0WC9OQnNyZz09"
-    },
-    "Конструкційні та електротехнічні матеріали": {
-        "teacher": "Віктор Лиманченко",
-        "classroom": "Код: zesv5q3",
-        "zoom": "https://us05web.zoom.us/j/88667130665?pwd=ZU9aSTBENTJRUm84WDFLb1VyMFNvZz09"
-    },
-    "Історія 9 клас": {
-        "teacher": "Не вказано",
-        "classroom": "https://classroom.google.com/c/NzA0MjY1MDQxNDIx?cjc=dhdtbsa",
-        "zoom": "https://us05web.zoom.us/j/82252074027?pwd=RUt5MzFUbUx3Q2FxcmcyK2xsM2lJdz09"
-    },
-    "Історія 11 клас": {
-        "teacher": "Не вказано",
-        "classroom": "https://classroom.google.com/c/NzA9NDI2NDk1Njg0?cjc=55cc5dj",
-        "zoom": "https://us05web.zoom.us/j/82252074027?pwd=RUt5MzFUbUx3Q2FxcmcyK2xsM2lJdz09"
-    },
-    "Правознавство": {
-        "teacher": "Мироненко Олена Юріївна",
-        "classroom": "https://classroom.google.com/c/ODc2NjE5MTkyMjEy?cjc=o3ft4pfb",
-        "zoom": "https://us05web.zoom.us/j/3321454583?pwd=WURKSlhUTGhDeFhEa09QU1Q4TmZkdz09"
-    },
-    "Англійська мова": {
-        "teacher": "Салманова Лариса Миколаївна",
-        "classroom": "https://classroom.google.com/c/ODY5MzQ0MDExNDk4?cjc=jwi4ysfh",
-        "zoom": "https://us04web.zoom.us/j/74528114620?pwd=BEPbNFBlqrsf7BHIqisgHYim4kbQwT.1"
-    },
-    "Математика": {
-        "teacher": "Гришко Галина Леонідівна",
-        "classroom": "https://classroom.google.com/c/ODc2NjExMTIxOTc5?cjc=aff7yt24",
-        "zoom": "https://us04web.zoom.us/j/79806870768?pwd=wscfwbeFgO4wbqmAxBJy4znUvOK5E0.1"
-    },
-    "Біологія і екологія": {
-        "teacher": "Репринцева Ніна Іванівна",
-        "classroom": "https://classroom.google.com/c/ODc2NjM0NDgyMjAy?cjc=pjsofk5y",
-        "zoom": "https://us04web.zoom.us/j/4316219745?pwd=MvEw06btO8M1LQIg11moihensLlJ9V.1"
-    }
-}
-
-# Расписание звонков (пары)
+# Расписание звонков
 CALLS = {
-    1: {"start": "08:00", "end": "09:20"},
-    2: {"start": "09:30", "end": "10:50"},
-    3: {"start": "11:10", "end": "12:30"},
-    4: {"start": "12:40", "end": "14:00"}
+    "1": {"start": "08:00", "end": "09:35"},  # Во вторник 1-я пара начинается в 09:00
+    "2": {"start": "09:45", "end": "11:20"},
+    "3": {"start": "12:00", "end": "13:35"},
+    "4": {"start": "13:45", "end": "15:20"}
 }
 
-# Пример расписания на неделю (его легко менять прямо здесь!)
+# Информация о предметах (ссылки на Zoom и Classroom можно будет вставить позже)
+SUBJECTS = {
+    "Виховна": {"teacher": "Не вказано", "zoom": "", "classroom": ""},
+    "Фізвиховання": {"teacher": "Не вказано", "zoom": "", "classroom": ""},
+    "Ін. мова 12а/преп": {"teacher": "Не вказано", "zoom": "", "classroom": ""},
+    "Укр. Мова": {"teacher": "Не вказано", "zoom": "", "classroom": ""},
+    "Біологія": {"teacher": "Не вказано", "zoom": "", "classroom": ""},
+    "Конструкційні матеріали": {"teacher": "Не вказано", "zoom": "", "classroom": ""},
+    "Фізика": {"teacher": "Не вказано", "zoom": "", "classroom": ""},
+    "Правознавство": {"teacher": "Не вказано", "zoom": "", "classroom": ""},
+    "Укр. Літ": {"teacher": "Не вказано", "zoom": "", "classroom": ""}
+}
+
+# Расписание по дням
 SCHEDULE = {
-    "Monday": {
-        1: "Біологія і екологія",
-        2: "Математика",
-        3: "Українська мова та література"
-    },
+    "Monday": {},  # В понедельник пар нет
+    
     "Tuesday": {
-        1: "Фізика",
-        2: "Англійська мова",
-        3: "Правознавство"
+        "1": "Виховна",
+        "2": "Фізвиховання",
+        "3": "Ін. мова 12а/преп",
+        "4": "Укр. Мова"
     },
+    
     "Wednesday": {
-        1: "Історія 9 клас",
-        2: "Конструкційні та електротехнічні матеріали"
+        "1": "Біологія",
+        "2": "Укр. Мова",
+        "3": "Конструкційні матеріали",
+        "4": "Фізика"
     },
+    
     "Thursday": {
-        1: "Математика",
-        2: "Фізика"
+        "1": "Конструкційні матеріали",
+        "2": "Правознавство",
+        "3": "Укр. Літ",
+        "4": "Фізвиховання"
     },
+    
     "Friday": {
-        1: "Виховні години та фізичне виховання",
-        2: "Історія 11 клас"
+        "1": "Конструкційні матеріали",
+        "2": "Біологія",
+        "3": "Укр. Мова",
+        "4": "Правознавство"
     }
 }
