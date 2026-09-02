c11b9c8d18b87096b69521c702b862516a97fe27
levadskiy18-byte <levadskiy18@gmail.com>
2026-09-02 09:50:19 +0300

    Update data.py

--- data.py
+++ data.py
@@ -1,4 +1,3 @@
-# Расписание звонков
 CALLS = {
     "1": {"start": "08:00", "end": "09:35"},
     "2": {"start": "09:45", "end": "11:20"},
@@ -6,101 +5,64 @@ CALLS = {
     "4": {"start": "13:45", "end": "15:20"}
 }
 
-
-# Информация о предметах, преподавателях и ссылках
 SUBJECTS = {
-
-    "Виховна": {
-        "teacher": "Шевченко Тетяна Валеріївна",
-        "zoom": "https://us05web.zoom.us/j/87000857117?pwd=VXduMys0V3FhanJQY0VxdUU0QllwQT09",
-        "classroom": "https://classroom.google.com/c/ODAxMDU3MTA0Mzk5?cjc=p4c75e2n"
-    },
-
-    "Фізвиховання": {
-        "teacher": "Шевченко Тетяна Валеріївна",
-        "zoom": "https://us05web.zoom.us/j/87000857117?pwd=VXduMys0V3FhanJQY0VxdUU0QllwQT09",
-        "classroom": "https://classroom.google.com/c/ODAxMDU3MTA0Mzk5?cjc=p4c75e2n"
-    },
-
-    "Ін. мова 12а/преп": {
-        "teacher": "Салманова Лариса Миколаївна",
-        "zoom": "https://us04web.zoom.us/j/74528114620?pwd=BEPbNFBlqrsf7BHIqisgHYim4kbQwT.1",
-        "classroom": "https://classroom.google.com/c/ODY5MzQ0MDExNDk4?cjc=jwi4ysfh"
-    },
-
-    "Укр. Мова": {
+    "Українська мова та література": {
         "teacher": "Бречко Олена Миколаївна",
         "zoom": "https://us04web.zoom.us/j/5546658153?pwd=eUR0cnF2Y3pIUHozeHhjV09XOU9OQT09",
         "classroom": "https://classroom.google.com/c/ODc2NDI2NTM0NzYw?cjc=j7foncpe"
     },
-
-    "Укр. Літ": {
-        "teacher": "Бречко Олена Миколаївна",
-        "zoom": "https://us04web.zoom.us/j/5546658153?pwd=eUR0cnF2Y3pIUHozeHhjV09XOU9OQT09",
-        "classroom": "https://classroom.google.com/c/ODc2NDI2NTM0NzYw?cjc=j7foncpe"
+    "Виховні години та фізичне виховання": {
+        "teacher": "Шевченко Тетяна Валеріївна",
+        "zoom": "https://us05web.zoom.us/j/87000857117?pwd=VXduMys0V3FhanJQY0VxdUU0QllwQT09",
+        "classroom": "https://classroom.google.com/c/ODAxMDU3MTA0Mzk5?cjc=p4c75e2n"
     },
-
-    "Біологія": {
-        "teacher": "Репринцева Ніна Іванівна",
-        "zoom": "https://us04web.zoom.us/j/4316219745?pwd=MvEw06btO8M1LQIg11moihensLlJ9V.1",
-        "classroom": "https://classroom.google.com/c/ODc2NjM0NDgyMjAy?cjc=pjsofk5y"
-    },
-
-    "Конструкційні матеріали": {
-        "teacher": "Віктор Лиманченко",
-        "zoom": "https://us05web.zoom.us/j/88667130665?pwd=ZU9aSTBENTJRUm84WDFLb1VyMFNvZz09",
-        "classroom": "zesv5q3"
-    },
-
     "Фізика": {
         "teacher": "Табачник Олена Миколаївна",
         "zoom": "https://us05web.zoom.us/j/5433319306?pwd=R1VQMFlHbFY3UGRSS0p0WC9OQnNyZz09",
         "classroom": "https://classroom.google.com/c/ODc2NTQ3Mjk0NTMx?cjc=42m7a3h6"
     },
-
+    "Конструкційні та електротехнічні матеріали": {
+        "teacher": "Віктор Лиманченко",
+        "zoom": "https://us05web.zoom.us/j/88667130665?pwd=ZU9aSTBENTJRUm84WDFLb1VyMFNvZz09",
+        "classroom": "https://classroom.google.com/c/zesv5q3"
+    },
+    "Історія 9 класс": {
+        "teacher": "Не вказано",
+        "zoom": "https://us05web.zoom.us/j/82252074027?pwd=RUt5MzFUbUx3Q2FxcmcyK2xsM2lJdz09",
+        "classroom": "https://classroom.google.com/c/NzA0MjY1MDQxNDIx?cjc=dhdtbsa"
+    },
+    "Історія 11 класс": {
+        "teacher": "Не вказано",
+        "zoom": "https://us05web.zoom.us/j/82252074027?pwd=RUt5MzFUbUx3Q2FxcmcyK2xsM2lJdz09",
+        "classroom": "https://classroom.google.com/c/NzA9NDI2NDk1Njg0?cjc=55cc5dj"
+    },
     "Правознавство": {
         "teacher": "Мироненко Олена Юріївна",
         "zoom": "https://us05web.zoom.us/j/3321454583?pwd=WURKSlhUTGhDeFhEa09QU1Q4TmZkdz09",
         "classroom": "https://classroom.google.com/c/ODc2NjE5MTkyMjEy?cjc=o3ft4pfb"
     },
-
+    "Англійська мова": {
+        "teacher": "Салманова Лариса Миколаївна",
+        "zoom": "https://us04web.zoom.us/j/74528114620?pwd=BEPbNFBlqrsf7BHIqisgHYim4kbQwT.1",
+        "classroom": "https://classroom.google.com/c/ODY5MzQ0MDExNDk4?cjc=jwi4ysfh"
+    },
     "Математика": {
         "teacher": "Гришко Галина Леонідівна",
         "zoom": "https://us04web.zoom.us/j/79806870768?pwd=wscfwbeFgO4wbqmAxBJy4znUvOK5E0.1",
         "classroom": "https://classroom.google.com/c/ODc2NjExMTIxOTc5?cjc=aff7yt24"
+    },
+    "Біологія і Екологія": {
+        "teacher": "Репринцева Ніна Іванівна",
+        "zoom": "https://us04web.zoom.us/j/4316219745?pwd=MvEw06btO8M1LQIg11moihensLlJ9V.1",
+        "classroom": "https://classroom.google.com/c/ODc2NjM0NDgyMjAy?cjc=pjsofk5y"
     }
 }
 
-
-# Расписание по дням
+# Сюда подставим расписание, когда пришлешь дни недели и пары
 SCHEDULE = {
     "Monday": {},
-
-    "Tuesday": {
-        "1": "Виховна",
-        "2": "Фізвиховання",
-        "3": "Ін. мова 12а/преп",
-        "4": "Укр. Мова"
-    },
-
-    "Wednesday": {
-        "1": "Біологія",
-        "2": "Укр. Мова",
-        "3": "Конструкційні матеріали",
-        "4": "Фізика"
-    },
-
-    "Thursday": {
-        "1": "Конструкційні матеріали",
-        "2": "Правознавство",
-        "3": "Укр. Літ",
-        "4": "Фізвиховання"
-    },
-
-    "Friday": {
-        "1": "Конструкційні матеріали",
-        "2": "Біологія",
-        "3": "Укр. Мова",
-        "4": "Правознавство"
-    }
+    "Tuesday": {},
+    "Wednesday": {},
+    "Thursday": {},
+    "Friday": {}
 }
